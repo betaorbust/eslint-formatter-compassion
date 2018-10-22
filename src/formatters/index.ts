b4daffacf6a62ab3ef2cb0b64b601bdee24fb92b
Jacques Favreau <betaorbust@users.noreply.github.com>
2018-10-21 22:17:22 -0700

    Deprecate formatters/index

--- src/formatters/index.ts
+++ src/formatters/index.ts
@@ -3,6 +3,11 @@ import codeframe from './codeframe';
 import visualstudio from './visualstudio';
 import { FormatFunction } from './formatter-types';
 
+// eslint-disable-next-line no-console
+console.warn(
+    'eslint-formatter-compassion/formatters/index has been deprecated and will be removed in the future. Please require formatters directly'
+);
+
 const formatters: { [key: string]: FormatFunction } = {
     stylish,
     codeframe,
